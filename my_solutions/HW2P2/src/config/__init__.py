93fd827063f78d43ea9b345f33a4169b081c8ab9
HDLiter <hdliter@gmail.com>
2025-07-30 00:56:10 +0800

    Refactor configuration module: remove unused methods and hyperparameter handling functions from ConfigManager

--- my_solutions/HW2P2/src/config/__init__.py
+++ my_solutions/HW2P2/src/config/__init__.py
@@ -2,7 +2,7 @@
 Configuration module for HW2P2
 """
 
-from .config_manager import ConfigManager, get_config, get_config_with_hyperparams
+from .config_manager import ConfigManager, get_config
 from .compat import config  # Backward compatibility
 
-__all__ = ["ConfigManager", "get_config", "get_config_with_hyperparams", "config"]
+__all__ = ["ConfigManager", "get_config", "config"]
