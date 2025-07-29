9b9d53fa4bee293a4f2190cbea8a1767a5fb02dd
HDLiter <hdliter@gmail.com>
2025-07-30 00:56:26 +0800

    Refactor models package init: remove ConvolutionBlock from exports

--- my_solutions/HW2P2/src/models/__init__.py
+++ my_solutions/HW2P2/src/models/__init__.py
@@ -3,6 +3,7 @@ Models package init file
 """
 
 from .architecture_factory import ArchitectureFactory
-from .common_blocks import ConvolutionBlock
 
-__all__ = ["ArchitectureFactory", "ConvolutionBlock"]
+__all__ = [
+    "ArchitectureFactory",
+]
